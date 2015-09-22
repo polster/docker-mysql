9f21405d87e65651384a4985930b67ddd46a8c1f
Simon Dietschi <simon.dietschi@bluecare.ch>
2015-09-22 13:43:58 +0200

    support for empty root password removed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,20 +8,16 @@ fi
 if [ "$1" = 'mysqld_safe' ]; then
 	DATADIR="/var/lib/mysql"
 
+	# if the DB file store has not been initialized
 	if [ ! -d "$DATADIR/mysql" ]; then
-		if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" ]; then
+		if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
 			echo >&2 'error: database is uninitialized and MYSQL_ROOT_PASSWORD not set'
-			echo >&2 '  Did you forget to add -e MYSQL_ROOT_PASSWORD=... ?'
 			exit 1
 		fi
 
 		echo 'Running mysql_install_db ...'
 		mysql_install_db --datadir="$DATADIR"
 		echo 'Finished mysql_install_db'
-
-		# These statements _must_ be on individual lines, and _must_ end with
-		# semicolons (no line breaks or comments are permitted).
-		# TODO proper SQL escaping on ALL the things D:
 
 		tempSqlFile='/tmp/mysql-first-time.sql'
 		cat > "$tempSqlFile" <<-EOSQL
